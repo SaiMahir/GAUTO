d04389f296735e277bdf9e4da4cb6abb6f8d4e07
SaiMahir <saimahirinukurthi@gmail.com>
2026-06-10 09:43:16 +0530

    edited popup

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 /**
- * Autofill login credentials on the college portal page
+ * Autofill login credentials on the GITAM login page
  */
 chrome.storage.sync.get(["username", "password"], (data) => {
   if (chrome.runtime.lastError) {
@@ -7,16 +7,26 @@ chrome.storage.sync.get(["username", "password"], (data) => {
     return;
   }
 
-  const userField = document.querySelector("#username");
-  const passField = document.querySelector("#password");
+  // Try multiple possible field selectors for GITAM login page
+  const userField = document.querySelector("input[name='username']")
+    || document.querySelector("input[name='userid']")
+    || document.querySelector("input[id='username']")
+    || document.querySelector("input[id*='user']");
+
+  const passField = document.querySelector("input[name='password']")
+    || document.querySelector("input[id='password']");
 
   try {
     if (userField && data.username) {
       userField.value = data.username;
+      userField.dispatchEvent(new Event('input', { bubbles: true }));
+      userField.dispatchEvent(new Event('change', { bubbles: true }));
     }
 
     if (passField && data.password) {
       passField.value = data.password;
+      passField.dispatchEvent(new Event('input', { bubbles: true }));
+      passField.dispatchEvent(new Event('change', { bubbles: true }));
     }
   } catch (error) {
     console.error("Error autofilling credentials:", error);
